f1497d119125f93bf1a75afbf4e92c333dd5cc43
Cassio de Souza <cassio.candango@gmail.com>
2024-03-29 20:34:20 +0800

    set up env variables

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
-    apiKey: 'AIzaSyA4ZCy-Yg4n_QAADB6Hz3hi3X0o_rKx580',
-    authDomain: 'chat-app-c460a.firebaseapp.com',
-    projectId: 'chat-app-c460a',
-    storageBucket: 'chat-app-c460a.appspot.com',
-    messagingSenderId: '772716247615',
-    appId: '1:772716247615:web:43712d9a8885f8cf2a968a',
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
